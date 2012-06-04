c890f4fd0e9c2d40304efbe5f4d05cae4ec2e48b
toeoso <toeoso@a7c52d6c-c931-11de-8672-e51b9359e43c>
2012-06-04 03:42:15 +0000

    增加blogtype分类

--- trunk/freewebsys.blog/src/test/java/com/freewebsys/blog/TestXMLConf.java
+++ trunk/freewebsys.blog/src/test/java/com/freewebsys/blog/TestXMLConf.java
@@ -14,6 +14,61 @@ import com.thoughtworks.xstream.io.xml.Dom4JDriver;
 
 public class TestXMLConf {
 
+	public static class TestObjType {
+		private String id;
+		private String name;
+
+		public String getId() {
+			return id;
+		}
+
+		public void setId(String id) {
+			this.id = id;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+	}
+
+	public static class TestObj {
+
+		private String name;
+
+		private String value;
+
+		private TestObjType type;
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public String getValue() {
+			return value;
+		}
+
+		public void setValue(String value) {
+			this.value = value;
+		}
+
+		public TestObjType getType() {
+			return type;
+		}
+
+		public void setType(TestObjType type) {
+			this.type = type;
+		}
+
+	}
+
 	/**
 	 * @param args
 	 * @throws IOException
@@ -22,30 +77,38 @@ public class TestXMLConf {
 	public static void main(String[] args) throws IOException {
 
 		XStream xStream = new XStream(new Dom4JDriver());
-		xStream.alias("Option", Option.class);
-		Map<String, Option> list = new HashMap<String, Option>();
+		xStream.alias("TestObj", TestObj.class);
+		Map<String, TestObj> list = new HashMap<String, TestObj>();
 		{
-			Option option = new Option();
-			option.setName("aa");
-			option.setValue("val");
-			list.put("0001", option);
+			TestObj testObj = new TestObj();
+			testObj.setName("aa");
+			testObj.setValue("val");
+			TestObjType objType = new TestObjType();
+			objType.setId("1111");
+			objType.setName("name001");
+			testObj.setType(objType);
+			list.put("0001", testObj);
 		}
 		{
-			Option option = new Option();
-			option.setName("aa");
-			option.setValue("<value>val</value>");
-			list.put("0002", option);
+			TestObj testObj = new TestObj();
+			testObj.setName("a222a");
+			testObj.setValue("<![CDATA[<value>val中文</value>]]>");
+			TestObjType objType = new TestObjType();
+			objType.setId("2222");
+			objType.setName("name0022");
+			testObj.setType(objType);
+			list.put("0002", testObj);
 		}
 		String out = xStream.toXML(list);
 		System.out.println(out);
-		xStream.toXML(list, new FileWriterWithEncoding(new File(
-				"testconf.xml"), "utf-8"));
+		xStream.toXML(list, new FileWriterWithEncoding(
+				new File("testconf.xml"), "utf-8"));
 
-		Map<String, Option> list2 = (Map<String, Option>) xStream
+		Map<String, TestObj> list2 = (Map<String, TestObj>) xStream
 				.fromXML(new File("testconf.xml"));
-		for (Map.Entry<String, Option> option : list2.entrySet()) {
-			System.out.println(option.getValue().getName() + ":"
-					+ option.getValue().getValue());
+		for (Map.Entry<String, TestObj> TestObj : list2.entrySet()) {
+			System.out.println(TestObj.getValue().getName() + ":"
+					+ TestObj.getValue().getValue());
 		}
 	}
 }
